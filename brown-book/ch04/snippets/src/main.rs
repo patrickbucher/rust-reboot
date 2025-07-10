26cf76450e562ec323f2cc311f99804c044f90a5
Patrick Bucher <patrick.bucher@mailbox.org>
2025-07-10 14:45:51 +0200

    example own ownership/reference passing

---
+++ brown-book/ch04/snippets/src/main.rs
@@ -0,0 +1,34 @@
+fn take_mutable_ownership(mut s: String) {
+    s.push_str(".");
+    println!("{s}");
+}
+
+fn take_ownership(s: String) {
+    println!("{s}.");
+}
+
+fn take_mutable_reference(s: &mut String) {
+    s.push_str(".");
+    println!("{s}");
+}
+
+fn take_referene(s: &String) {
+    println!("{s}.");
+}
+
+fn main() {
+    let mut a = String::from("a");
+    let mut b = String::from("b");
+    let mut c = String::from("c");
+    let mut d = String::from("d");
+
+    take_mutable_ownership(a);
+    take_ownership(b);
+    take_mutable_reference(&mut c);
+    take_referene(&d);
+
+    // println!("{a}"); // fails
+    // println!("{b}"); // fails
+    println!("{c}"); // ok
+    println!("{d}"); // ok
+}
